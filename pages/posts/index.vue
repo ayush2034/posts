d1340211f25517f53d0044cc7ec7512518ecbb24
Ayush Kumar <ayush.kumar@summence.com>
2025-08-13 00:24:14 +0530

    first setup commit

---
+++ pages/posts/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <p class="bg-blue-300">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officia, ut,
+    veniam vel, aliquam consequatur fuga illo exercitationem optio neque error
+    ad? Ut temporibus aperiam cumque reiciendis harum ullam et?
+  </p>
+</template>
